522dc14c9dc96bf6ac1a2b611240934793017cc8
abdc06 <abdc2806@gmail.com>
2022-11-12 22:40:24 +0900

    controller control

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Query, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,17 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @HttpCode(202)
+  @Header('Cache-Control', 'none')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+      if (version && version === '5') {
+          return { url: 'https://docs.nestjs.com/v5/' };
+      }
   }
 }
